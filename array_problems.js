a5e33d8d2a91a096cba224744dbffa7ff257ac6c
Akila <suyambuakila1709@gmail.com>
2026-01-31 17:19:22 +0530

    31-01-2026

--- array_problems.js
+++ array_problems.js
@@ -10,7 +10,7 @@ function largest() {
     }
     return max;
 
-}console.log("Largest number in the array:" + largest());
+} console.log("Largest number in the array:" + largest());
 //summ of array elements
 function sumofarray() {
     let arr = [1, 4, 5, 5, 2];
@@ -29,18 +29,58 @@ console.log("Sum of the Array:" + sumofarray());
 
 // Return any one pair” → [0,3] or [1,2] both are correct    [it'll give first pair...]
 
-function insertion(){
-    let input=[1,2,3,4];
-    let target=5;
-    for(let i=0;i<input.length;i++){
-        for(j=i+1;j<input.length;j++){
-            if(input[i]+input[j]===target)
-                return [i,j];
+function insertion() {
+    let input = [1, 2, 3, 4];
+    let target = 5;
+    for (let i = 0; i < input.length; i++) {
+        for (j = i + 1; j < input.length; j++) {
+            if (input[i] + input[j] === target)
+                return [i, j];
         }
     }
 }
 
-console.log("Expected output:"+insertion());
+console.log("Expected output:" + insertion());
+
+
+// count event numbers
+function countEven() {
+    let arr = [2, 4, 3, 1, 6];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log("Total Even Numbers:" + countEven());
+
+// reverse an String without Reverse()
+
+function reverseAnString() {
+    let arr = "Hello"
+    let after = "";
+    for (let i = arr.length - 1; i >= 0; i--) {
+        after += arr[i];
+    }
+    return after;
+
+}
+console.log("Reversing An Array\t:"+reverseAnString())
+
+// reversing an Array
+function reverseAnArray(){
+    let arr=[1,4,5,6];
+    let after=[];
+    for(let i=arr.length-1;i>=0;i--){
+        after.push(arr[i]);
+    }
+    return after;
+
+}
+console.log("Reversing An Array\t:",reverseAnArray());
+
 
 
 
